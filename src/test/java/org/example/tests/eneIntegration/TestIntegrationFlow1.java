cec5cebd8b24622b9de2140f61eb5dca4400a265
srikanth rangavarjula <srikanthrangavarula@gmail.com>
2026-02-19 23:59:32 +0530

    E2E workflow

--- src/test/java/org/example/tests/eneIntegration/TestIntegrationFlow1.java
+++ src/test/java/org/example/tests/eneIntegration/TestIntegrationFlow1.java
@@ -1,8 +1,10 @@
 package org.example.tests.eneIntegration;
 
+import com.google.gson.Gson;
 import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import org.example.Endpoints.APIConstants_URLs;
+import org.example.POJOs.request.Booking;
 import org.example.POJOs.response.BookingResponse;
 import org.example.base.BaseClass_CommonTOAll;
 import org.testng.Assert;
@@ -40,26 +42,38 @@ public class TestIntegrationFlow1 extends BaseClass_CommonTOAll {
 
     @Test(groups = "qa", priority = 2)
     @Description("TC#2 verify the booking by ID ")
-    public void testVerifybookingbyID()
+    public void testVerifybookingbyID(ITestContext ITestcontext)
     {
-        System.out.println();
+        Integer bookingid = (Integer) ITestcontext.getAttribute("bookingid");
+        requestSpecification.basePath(APIConstants_URLs.Create_update_bookingURL + "/" + bookingid);
+        response = RestAssured.given(requestSpecification).when().get();
+        Booking booking = new Gson().fromJson(response.asString(), Booking.class);
+        assertactions.Verifystringkey(booking.getFirstname(), "Srikanth");
     }
-
-
-
 
     @Test(groups = "qa", priority = 3)
     @Description("TC#3 verify updated by booking ID ")
-    public void testupdatebookingbyID(){
-        Assert.assertTrue(true);
+    public void testupdatebookingbyID(ITestContext ITestcontext){
+        String token = RestAssured.given().baseUri(APIConstants_URLs.base_url)
+                .basePath(APIConstants_URLs.AUTH_URL).contentType("application/json")
+                .body(payloadManager.setAuthPayload()).post().then().extract().path("token");
+        Integer bookingid = (Integer) ITestcontext.getAttribute("bookingid");
+        requestSpecification.basePath(APIConstants_URLs.Create_update_bookingURL + "/" + bookingid);
+        response = RestAssured.given(requestSpecification).cookie("token", token)
+                .when().body(payloadManager.CreatepayloadBookingAsString()).put();
+        //assertactions.VerifyStatusCode(response.getStatusCode(), 200);
     }
-
-
 
     @Test(groups = "qa", priority = 4)
     @Description("TC#4 verify delete by booking ID ")
-    public void testdeletebookingbyID(){
-        Assert.assertTrue(true);
+    public void testdeletebookingbyID(ITestContext ITestcontext){
+        String token = RestAssured.given().baseUri(APIConstants_URLs.base_url)
+                .basePath(APIConstants_URLs.AUTH_URL).contentType("application/json")
+                .body(payloadManager.setAuthPayload()).post().then().extract().path("token");
+        Integer bookingid = (Integer) ITestcontext.getAttribute("bookingid");
+        requestSpecification.basePath(APIConstants_URLs.Create_update_bookingURL + "/" + bookingid);
+        response = RestAssured.given(requestSpecification).cookie("token", token).when().delete();
+        //assertactions.VerifyStatusCode(response.getStatusCode(), 201);
     }
 
 
